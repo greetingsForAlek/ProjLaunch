35f06eecfe80e6cc667f662526460f81cff7b32c
greetingsForAlek <alexpwoods@outlook.com>
2026-05-30 13:48:05 +0200

    Save Config and Add Project

--- ProjLaunch.py
+++ ProjLaunch.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 import json
+import sys
+
+config_dir = Path.home() / ".projlauncher"
+config_file = config_dir / "projects.json"
+
 
 def get_config():
-    config_dir = Path.home() / ".projlauncher"
-    config_file = config_dir / "projects.json"
 
     config_dir.mkdir(exist_ok=True)
 
@@ -13,3 +16,36 @@ def get_config():
 
     with open(config_file, "r") as f:
         return json.load(f)
+
+
+def save_config(config):
+    with open(config_file, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+def add_project():
+    config = get_config()
+    name = input("Project name > ")
+
+    if name in config["projects"]:
+        print("Project already exists.")
+        return
+
+    path = input("Project Path > ")
+
+    config["projects"][name] = {
+        "path": path,
+        "commands": [],
+        "urls": []
+    }
+
+    save_config(config)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    if sys.argv[1] == "add":
+        add_project()
+    elif sys.argv[1] == "launch":
+        pass
